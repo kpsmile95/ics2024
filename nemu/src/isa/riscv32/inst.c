08cd637e452004ba85d48dcc1e121aacfc9daae2
tracer-ics2024 <tracer@njuics.org>
2025-01-02 17:30:02 +0800

    >  compile NEMU 231220000 张三 Linux kp-machine 6.9.3-76060903-generic #202405300957~1732141768~22.04~f2697e1 SMP PREEMPT_DYNAMIC Wed N x86_64 x86_64 x86_64 GNU/Linux  17:30:02 up  3:16,  1 user,  load average: 0.35, 0.48, 0.46

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,13 +24,20 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J, TYPE_B// none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immJ() do { *imm = SEXT(\
+  (\
+    BITS(i, 31, 31) << 20 | \
+    BITS(i, 21, 21) << 1  | \
+    BITS(i, 20, 20) << 11 | \
+    BITS(i, 19, 12) << 12 \
+  ), 21); } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -41,6 +48,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
+    case TYPE_J:                   immJ(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
@@ -67,6 +75,8 @@ static int decode_exec(Decode *s) {
 
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", jal    , J, R(rd) = s->pc + imm);
 
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 100 ????? 00100 11", xori   , I, R(rd) = R(src1) ^ imm);
