464e474b1e043cbe5b40cf61f3fe9efc82dd43f2
tracer-ics2024 <tracer@njuics.org>
2025-01-06 15:21:15 +0800

    >  compile NEMU 231220000 张三 Linux kp-machine 6.9.3-76060903-generic #202405300957~1732141768~22.04~f2697e1 SMP PREEMPT_DYNAMIC Wed N x86_64 x86_64 x86_64 GNU/Linux  15:21:15 up  6:03,  1 user,  load average: 1.22, 1.32, 0.89

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -94,12 +94,53 @@ int sprintf(char *str, const char *format, ...) {
     return result;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-    panic("Not implemented");
+int vsnprintf(char *str, size_t size, const char *format, va_list args) {
+    int i = 0, j = 0;
+    char buffer[100];
+    char *str_arg;
+    int int_arg;
+
+    while (format[i] != '\0' && j < size - 1) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+            case 'd':
+                int_arg = va_arg(args, int);
+                itoa(int_arg, buffer);
+                for (int k = 0; buffer[k] != '\0' && j < size - 1; k++) {
+                    str[j++] = buffer[k];
+                }
+                break;
+            case 's':
+                str_arg = va_arg(args, char *);
+                while (*str_arg && j < size - 1) {
+                    str[j++] = *str_arg++;
+                }
+                break;
+            // 可以根据需要添加更多格式化选项
+            default:
+                if (j < size - 1) {
+                    str[j++] = format[i];
+                }
+                break;
+            }
+        } else {
+            if (j < size - 1) {
+                str[j++] = format[i];
+            }
+        }
+        i++;
+    }
+    str[j] = '\0';
+    return j;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-    panic("Not implemented");
+int snprintf(char *str, size_t size, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int result = vsnprintf(str, size, format, args);
+    va_end(args);
+    return result;
 }
 
 #endif
