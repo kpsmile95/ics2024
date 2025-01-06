a153db8621041246526b179426df33a543d97d80
tracer-ics2024 <tracer@njuics.org>
2025-01-06 15:18:43 +0800

    >  compile NEMU 231220000 张三 Linux kp-machine 6.9.3-76060903-generic #202405300957~1732141768~22.04~f2697e1 SMP PREEMPT_DYNAMIC Wed N x86_64 x86_64 x86_64 GNU/Linux  15:18:43 up  6:00,  1 user,  load average: 1.54, 1.24, 0.78

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,105 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+// 辅助函数：将整数转换为字符串
+static void itoa(int value, char *str) {
+    char *p = str;
+    char *p1, *p2;
+    int tmp_value;
+
+    if (value < 0) {
+        value = -value;
+        *p++ = '-';
+    }
+
+    p1 = p;
+    do {
+        tmp_value = value;
+        value /= 10;
+        *p++ = '0' + (tmp_value - value * 10);
+    } while (value);
+
+    *p = '\0';
+
+    // 反转字符串
+    p2 = p - 1;
+    while (p1 < p2) {
+        char tmp = *p1;
+        *p1 = *p2;
+        *p2 = tmp;
+        p1++;
+        p2--;
+    }
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int printf(const char *format, ...) {
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    int result = vsprintf(buffer, format, args);
+    va_end(args);
+    // 输出到标准输出
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        putch(buffer[i]);
+    }
+    return result;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int vsprintf(char *str, const char *format, va_list args) {
+    int i = 0, j = 0;
+    char buffer[100];
+    char *str_arg;
+    int int_arg;
+
+    while (format[i] != '\0') {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+            case 'd':
+                int_arg = va_arg(args, int);
+                itoa(int_arg, buffer);
+                for (int k = 0; buffer[k] != '\0'; k++) {
+                    str[j++] = buffer[k];
+                }
+                break;
+            case 's':
+                str_arg = va_arg(args, char *);
+                while (*str_arg) {
+                    str[j++] = *str_arg++;
+                }
+                break;
+            // 可以根据需要添加更多格式化选项
+            default:
+                str[j++] = format[i];
+                break;
+            }
+        } else {
+            str[j++] = format[i];
+        }
+        i++;
+    }
+    str[j] = '\0';
+    return j;
+}
+
+int sprintf(char *str, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int result = vsprintf(str, format, args);
+    va_end(args);
+    return result;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 #endif
